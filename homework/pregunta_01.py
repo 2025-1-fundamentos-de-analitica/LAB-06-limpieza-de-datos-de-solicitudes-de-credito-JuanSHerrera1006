a22e268215e7bbdd441c7217be93c0cab8551c25
Juan Sebastian Herrera Gomez <juherrerago@unal.edu.co>
2025-07-14 07:38:15 -0500

    feat(*): Solucion LAB #6

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,18 @@
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
+import pandas as pd
+import os
+
+INPUT_PATH = "files/input/"
+OUTPUT_PATH = "files/output/"
+
+
+def clean_string(s: str):
+    tmp = s.upper()
+    ans = tmp.replace("_", " ").replace(".", " ").replace("-", " ")
+    return " ".join(ans.split()).strip()
+
 
 def pregunta_01():
     """
@@ -13,3 +25,47 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    df_credit_req = pd.read_csv(
+        os.path.join(INPUT_PATH, "solicitudes_de_credito.csv"), sep=";", index_col=0
+    )
+
+    df_clean = df_credit_req.copy().dropna()
+
+    string_cols = [
+        "sexo",
+        "tipo_de_emprendimiento",
+        "idea_negocio",
+        "línea_credito",
+    ]
+
+    for col in string_cols:
+        df_clean[col] = df_clean[col].apply(clean_string).astype("category")
+
+    df_clean["monto_del_credito"] = (
+        df_clean["monto_del_credito"]
+        .str.replace("$ ", "")
+        .str.replace(",", "")
+        .astype(float)
+    )
+
+    df_clean["estrato"] = df_clean["estrato"].astype("category")
+
+    df_clean["barrio"] = (
+        df_clean["barrio"]
+        .str.upper()
+        .str.replace("_", " ")
+        .str.replace("-", " ")
+        .astype("category")
+    )
+
+    df_clean["fecha_de_beneficio"] = pd.to_datetime(
+        df_clean["fecha_de_beneficio"], dayfirst=True, format="mixed"
+    )
+
+    df_clean.drop_duplicates(inplace=True)
+
+    # Cargar resultados
+    df_clean.to_csv(
+        os.path.join(OUTPUT_PATH, "solicitudes_de_credito.csv"), sep=";", index=False
+    )
+    return df_clean
